76368d035c9ff9bee40a070d03ce4a6250cb8d56
hongymagic <davidhong.code@gmail.com>
2013-01-18 18:17:53 +1100

    Clean up whitespace

--- jQuery.serializeObject.js
+++ jQuery.serializeObject.js
@@ -1,10 +1,10 @@
 //
 // Use internal $.serializeArray to get list of form elements which is consistent with $.serialize
 //
-// And to avoid names such as 
+// And to avoid names such as
 // => object["favorite-color"]
 //
-// We camelcase the name part, so the notation becomes 
+// We camelcase the name part, so the notation becomes
 // => object["favoriteColor"]
 //
 // Conveniently, this allows period notation to be used.
@@ -16,30 +16,27 @@
 // => { favoriteColor: 'yellow' }
 //
 $.fn.serializeObject = function () {
-  var 
-
-    result = Object.create(null),
-    mapper = function (element) {
-      element.name = $.camelCase(element.name);
-      return element;
-    },
-    extend = function (i, element) {
-      var node = result[element.name];
+	var result = Object.create(null);
+	var mapper = function (element) {
+		element.name = $.camelCase(element.name);
+		return element;
+	};
+	var extend = function (i, element) {
+		var node = result[element.name];
 
 // If node with same name exists already, need to convert it to an array as it
 // is a multi-value field (i.e., checkboxes)
 
-      if ('undefined' !== typeof node && node !== null) {
-        result[element.name] = node.push ? node.push(element.value) : [node, element.value];
-      } else {
-        result[element.name] = element.value;
-      }
-    };
+		if ('undefined' !== typeof node && node !== null) {
+			result[element.name] = node.push ? node.push(element.value) : [node, element.value];
+		} else {
+			result[element.name] = element.value;
+		}
+	};
 
 // For each serialzable element, convert element names to camelCasing and
 // extend each of them to a JSON object
 
-  $.each($.map(this.serializeArray(), mapper), extend);
-  return result;
+	$.each($.map(this.serializeArray(), mapper), extend);
+	return result;
 };
-
